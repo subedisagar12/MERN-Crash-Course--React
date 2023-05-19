3edee821cc88002537a7aaeeccd3a0e925796cd0
subedisagar12 <subedisagar12@gmail.com>
2023-05-19 20:21:51 +0545

    Blog fetching and deletion has been completed

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,15 +1,76 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function BlogList() {
+  // State to store blogs
+  const [allBlogs, setAllBlogs] = useState([]);
+
+  // Function to fetch all blogs from the server
+  function getAllBlogs() {
+    axios({
+      method: "GET",
+      url: "http://localhost:8000/blog/get",
+    })
+      .then((res) => {
+        console.log(res.data.data);
+        setAllBlogs(res.data.data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }
+
+  // Function to delete the blog
+
+  function deleteBlog(blogId) {
+    let confirmation = confirm("Do you want to delete this blog?");
+
+    if (confirmation) {
+      axios({
+        method: "DELETE",
+        url: "http://localhost:8000/blog/delete/" + blogId,
+      })
+        .then(() => {
+          alert("Blog has been deleted successfully");
+          // window.location.reload();
+
+          // Updating the allBlogs state by removing the deleted blog
+          let remainingBlogs = allBlogs.filter((blog) => blog._id !== blogId);
+          setAllBlogs(remainingBlogs);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }
+
+  // Hook to execute the function once
+  useEffect(() => {
+    getAllBlogs();
+  }, []);
+
   return (
     <>
-      <div className="card">
-        <div className="card-body">
-          <h5 className="card-title">Blog title</h5>
+      {allBlogs.map((blog) => {
+        return (
+          <div className="card my-4" key={blog._id}>
+            <div className="card-body">
+              <h5 className="card-title">{blog.title}</h5>
 
-          <p className="card-text">Blog description goes here</p>
+              <p className="card-text">{blog.description}</p>
 
-          <button className="btn btn-danger btn-sm">Delete</button>
-        </div>
-      </div>
+              <button
+                className="btn btn-danger btn-sm"
+                onClick={() => {
+                  deleteBlog(blog._id);
+                }}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        );
+      })}
     </>
   );
 }
